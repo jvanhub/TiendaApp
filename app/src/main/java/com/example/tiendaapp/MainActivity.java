e7cb192fb586d4735b6df4cf8f5fec9a18dcc293
Iván <ivan.m.diaz@hotmail.es>
2021-10-13 16:38:51 +0200

    Nueva activity bienvenido

--- app/src/main/java/com/example/tiendaapp/MainActivity.java
+++ app/src/main/java/com/example/tiendaapp/MainActivity.java
@@ -8,22 +8,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
-    private EditText numTelf;
+    private EditText email;
     private EditText pass;
     private Button entrar;
     private Button registrar;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        numTelf = findViewById(R.id.editTextNumber);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.editTextNumber);
         pass = findViewById(R.id.editTextTextPass);
+        entrar = (Button) findViewById(R.id.buttonEntrar);
 
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this, Bienvenido.class));
+            }
+        });
     }
-    public void entrar(View view){
 
-    }
     public void registrar(View view){
         startActivity(new Intent(MainActivity.this,Formulario.class));
     }
