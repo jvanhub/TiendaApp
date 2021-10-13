107105c5d732203494a9e695aaca9bd3f899251b
jvanhub <ivan.m.diaz@hotmail.es>
2021-10-13 18:34:18 +0200

    Okk

--- app/src/main/java/com/example/tiendaapp/Bienvenido.java
+++ app/src/main/java/com/example/tiendaapp/Bienvenido.java
@@ -21,7 +21,7 @@ public class Bienvenido extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenido);
 
-      /*  mAuth = FirebaseAuth.getInstance();
+       mAuth = FirebaseAuth.getInstance();
         cerrar_sesion = (Button) findViewById(R.id.buttonCerrarSession);
 
         cerrar_sesion.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +31,6 @@ public class Bienvenido extends AppCompatActivity {
                 startActivity(new Intent(Bienvenido.this, MainActivity.class));
                 finish();
             }
-        });*/
+        });
     }
 }
