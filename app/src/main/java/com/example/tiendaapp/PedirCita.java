79b3824bf4774ed691348dfb672e60dd5360ad7a
Iván <ivan.m.diaz@hotmail.es>
2021-10-14 14:14:12 +0200

    Nueva activity pedircita

--- app/src/main/java/com/example/tiendaapp/PedirCita.java
+++ app/src/main/java/com/example/tiendaapp/PedirCita.java
@@ -1,28 +1,42 @@
 package com.example.tiendaapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PedirCita extends AppCompatActivity {
     private Button pedirFecha, pedirHora, confirmar;
     private EditText fecha,hora;
     private int dia,mes,anyo;
+    private String fechaCompletaTv="";
 
-    private String fechadata="";
-
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,17 +61,22 @@ public class PedirCita extends AppCompatActivity {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                         fecha.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+
+                        fechaCompletaTv = fecha.getText().toString();
+                        Date fecha = null;
+                        try {
+                            fecha = sdf.parse(fechaCompletaTv);
+                        } catch (ParseException e) {
+                            Toast.makeText(PedirCita.this,"error fecha", Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                        }
+
                     }
                 }
                 ,dia,mes,anyo);
                 datePikerDialog.show();
             }
         });
-        pedirFecha.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
     }
 }
