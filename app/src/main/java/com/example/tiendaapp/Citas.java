619bf6ff923ceded00ff3749aee357f87cbb5878
Iván <ivan.m.diaz@hotmail.es>
2021-10-20 17:09:40 +0200

    Validaciones fecha hora

--- app/src/main/java/com/example/tiendaapp/Citas.java
+++ app/src/main/java/com/example/tiendaapp/Citas.java
@@ -4,13 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -18,12 +16,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Citas extends AppCompatActivity {
-    private Button verCita;
     private String fechaBBDD="";
     private String horaBBDD="";
     private String uId="";
@@ -31,6 +28,7 @@ public class Citas extends AppCompatActivity {
     FirebaseUser mAuth;
     DatabaseReference mDatabase;
     List<ListElemnt> elements;
+    List<ListElemnt> elements2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +38,23 @@ public class Citas extends AppCompatActivity {
         mAuth=FirebaseAuth.getInstance().getCurrentUser();
         mDatabase= FirebaseDatabase.getInstance().getReference();
         id = mAuth.getUid();
-        verCita = (Button) findViewById(R.id.buttonVerCitas);
+        Button verCita = (Button) findViewById(R.id.buttonVerCitas);
+        Button verCitaActual = (Button) findViewById(R.id.buttonVerCitasActules);
         elements = new ArrayList<>();
+        elements2 = new ArrayList<>();
 
         verCita.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 recogerCitas();
+            }
+        });
+
+        verCitaActual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                recogerCitasActualizadas();
             }
         });
     }
@@ -60,11 +68,11 @@ public class Citas extends AppCompatActivity {
                     mDatabase.child("Reservas").child(snapshot.getKey()).addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
+
                             fechaBBDD=snapshot.child("fecha").getValue().toString();
                             horaBBDD=snapshot.child("hora").getValue().toString();
                             uId=snapshot.child("uId").getValue().toString();
 
-      /**  Añadir acción sino hay citas y dar funcionalidad a botones y si es necesario incluir Volver atras**/
                             if (uId.equals(id)){
                                 insertElements();
                             }
@@ -77,12 +85,10 @@ public class Citas extends AppCompatActivity {
                 }
                 elements.clear();
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(Citas.this,"Error BBDD",Toast.LENGTH_LONG).show();
             }
-
         });
     }
 
@@ -90,6 +96,55 @@ public class Citas extends AppCompatActivity {
     public void insertElements(){
         elements.add(new ListElemnt(fechaBBDD,horaBBDD));
         ListAdapter listAdapter =  new ListAdapter(elements,this);
+        RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
+    }
+    public void recogerCitasActualizadas(){
+        mDatabase.child("Reservas").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Calendar calendario = Calendar.getInstance();
+                int dia =calendario.get(Calendar.DAY_OF_MONTH);
+                int mes = (calendario.get(Calendar.MONTH)+1);
+                int anyo = calendario.get(Calendar.YEAR);
+
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    mDatabase.child("Reservas").child(snapshot.getKey()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            fechaBBDD=snapshot.child("fecha").getValue().toString();
+                            horaBBDD=snapshot.child("hora").getValue().toString();
+                            uId=snapshot.child("uId").getValue().toString();
+                            String extractFecha[] = fechaBBDD.split("/");
+
+                            if (uId.equals(id)){
+                                if(Integer.parseInt(extractFecha[2]) - anyo < 0){
+                                }else if(Integer.parseInt(extractFecha[1]) - mes< 0){
+                                }else if(Integer.parseInt(extractFecha[0]) - dia< 0){
+                                }else {
+                                    insertElementsActual();
+                                }
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Toast.makeText(Citas.this,"Error BBDD",Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+                elements2.clear();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Citas.this,"Error BBDD",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    public void insertElementsActual(){
+        elements2.add(new ListElemnt(fechaBBDD,horaBBDD));
+        ListAdapter2 listAdapter =  new ListAdapter2(elements2,this);
         RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
--- app/src/main/java/com/example/tiendaapp/ListAdapter2.java
+++ app/src/main/java/com/example/tiendaapp/ListAdapter2.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,14 +13,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 //Esta clase relaciona la parte grafica con los datos (Fecha y Hora) que vamos ha tratar.
-public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+public class ListAdapter2 extends RecyclerView.Adapter<ListAdapter2.ViewHolder> {
 
     private List<ListElemnt>mData;//Esta lista tiene todos los datos de ListElement.
     private final LayoutInflater mInflater;//Describe de que archivo proviene.
     public final Context context;//Define de que clase estamos llamando este adaptador.
 
     //Constructor.
-    public ListAdapter(List<ListElemnt> itemList, Context context){
+    public ListAdapter2(List<ListElemnt> itemList, Context context){
         this.mInflater = LayoutInflater.from(context);
         this.context=context;
         this.mData = itemList;
@@ -28,14 +29,14 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     //A continuación damos la referencia de como se va a ver cada "tarjeta"
     @NonNull
     @Override
-    public ListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.list_element, null);
+    public ListAdapter2.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.list_element_actual, null);
         return new ViewHolder(view);
     }
 
     //
     @Override
-    public void onBindViewHolder(@NonNull ListAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ListAdapter2.ViewHolder holder, int position) {
         holder.bindData(mData.get(position));
     }
 
@@ -52,12 +53,16 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
         TextView mostrar, fecha, hora;
+        Button btMod, btElim;
 
         ViewHolder(View itemView){
             super(itemView);
-            mostrar = itemView.findViewById(R.id.textViewCitasCard);
-            fecha = itemView.findViewById(R.id.textViewFechaCard);
-            hora = itemView.findViewById(R.id.textViewHoraCard);
+            mostrar = itemView.findViewById(R.id.textViewCitasCard2);
+            fecha = itemView.findViewById(R.id.textViewFechaCard2);
+            hora = itemView.findViewById(R.id.textViewHoraCard2);
+            btMod = itemView.findViewById(R.id.buttonModifi);
+            btElim = itemView.findViewById(R.id.buttonDelet);
+
         }
 
         void bindData(final ListElemnt item){
