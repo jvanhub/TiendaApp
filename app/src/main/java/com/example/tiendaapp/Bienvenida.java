a5f4334d9fcd142408df9d4b8e786164ea2164b4
jvanhub <ivan.m.diaz@hotmai.es>
2021-11-04 11:52:23 +0100

    Perfil

--- app/src/main/java/com/example/tiendaapp/Bienvenida.java
+++ app/src/main/java/com/example/tiendaapp/Bienvenida.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,6 +23,15 @@ public class Bienvenida extends AppCompatActivity {
         Button pedir_cita = (Button) findViewById(R.id.button_pedir);
         Button ver_cita = (Button) findViewById(R.id.button_ver);
         Button cerrar_sesion = (Button) findViewById(R.id.buttonCerrarSession);
+        Button perfil = (Button) findViewById(R.id.button_perfil);
+
+        /*Evento para pasar al activity  */
+        perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Bienvenida.this, Perfil.class));
+            }
+        });
 
         /*Para dejarlo mejor lo convertimos:
         public void onClick(View v) {
