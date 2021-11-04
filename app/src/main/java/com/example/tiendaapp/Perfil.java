1e614f2a01bf22d63e878ff5b31ca3b81338449b
jvanhub <ivan.m.diaz@hotmai.es>
2021-11-04 12:01:09 +0100

    Perfil

--- app/src/main/java/com/example/tiendaapp/Perfil.java
+++ app/src/main/java/com/example/tiendaapp/Perfil.java
@@ -3,13 +3,29 @@ package com.example.tiendaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Perfil extends AppCompatActivity {
+    private Button btConfir, btVolver;
+    private EditText etNombre, etAp1, etAp2, etTelf, etEail, etEmailConf;
+    private TextView etInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        btConfir = (Button) findViewById(R.id.buttonModConfir);
+        btVolver = (Button) findViewById(R.id.buttonModVolver);
+
+        etNombre = (EditText) findViewById(R.id.editTextModNombre);
+        etAp1 = (EditText) findViewById(R.id.editTextApe1);
+        etAp2 = (EditText) findViewById(R.id.editTextModApe2);
+        etTelf = (EditText) findViewById(R.id.editTextModPhone);
+        etEail = (EditText) findViewById(R.id.editTextTextMailMod);
+        etEmailConf = (EditText) findViewById(R.id.editTextTextMailModCon);
     }
 
 
